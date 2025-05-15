f45933bb029ff12b376a67b3f544bd6c80d0968c
Joseph Schiarizzi <9449596+cupOJoseph@users.noreply.github.com>
2025-05-15 23:57:57 +0200

    add delegation function to complex erc20 pool

--- packages/contracts/src/contracts/implementations/PrivacyPoolComplexDelegate.sol
+++ packages/contracts/src/contracts/implementations/PrivacyPoolComplexDelegate.sol
@@ -16,42 +16,67 @@ https://defi.sucks/
 
 */
 
+interface IERC20Votes {
+  function delegate(address delegatee) external;
+}
+
+import {IERC20, SafeERC20} from '@oz/token/ERC20/utils/SafeERC20.sol';
+
 import {Constants} from 'contracts/lib/Constants.sol';
 
+import {IPrivacyPoolComplex} from 'interfaces/IPrivacyPool.sol';
+
 import {PrivacyPool} from 'contracts/PrivacyPool.sol';
-import {IPrivacyPoolSimple} from 'interfaces/IPrivacyPool.sol';
 
 /**
- * @title PrivacyPoolSimple
- * @notice Native asset implementation of Privacy Pool.
+ * @title PrivacyPoolComplex
+ * @notice ERC20 implementation of Privacy Pool.
  */
-contract PrivacyPoolSimple is PrivacyPool, IPrivacyPoolSimple {
+contract PrivacyPoolComplex is PrivacyPool, IPrivacyPoolComplex {
+  using SafeERC20 for IERC20;
+
+  address public delegateAddress;
+
   // @notice Initializes the state addresses
   constructor(
     address _entrypoint,
     address _withdrawalVerifier,
-    address _ragequitVerifier
-  ) PrivacyPool(_entrypoint, _withdrawalVerifier, _ragequitVerifier, Constants.NATIVE_ASSET) {}
+    address _ragequitVerifier,
+    address _asset,
+    address _delegateAddress
+  ) PrivacyPool(_entrypoint, _withdrawalVerifier, _ragequitVerifier, _asset) {
+    if (_asset == Constants.NATIVE_ASSET) revert NativeAssetNotSupported();
 
-  /**
-   * @notice Handle receiving native asset asset
-   * @param _amount The amount of asset receiving
-   * @inheritdoc PrivacyPool
-   */
-  function _pull(address, uint256 _amount) internal override(PrivacyPool) {
-    // Check the amount matches the value sent
-    if (msg.value != _amount) revert InsufficientValue();
+    delegateAddress = _delegateAddress;
   }
 
   /**
-   * @notice Handle sending native asset
+   * @notice Handle pulling an ERC20 asset
+   * @param _sender The address of the user transferring the asset from
+   * @param _amount The amount of asset being pulled
+   * @inheritdoc PrivacyPool
+   */
+  function _pull(address _sender, uint256 _amount) internal override(PrivacyPool) {
+    // This contract does not accept native asset
+    if (msg.value != 0) revert NativeAssetNotAccepted();
+
+    // Pull asset from sender to this contract
+    IERC20(ASSET).safeTransferFrom(_sender, address(this), _amount);
+  }
+
+  /**
+   * @notice Handle sending an ERC20 asset
    * @param _recipient The address of the user receiving the asset
-   * @param _amount The amount of native asset being sent
+   * @param _amount The amount of asset being sent
    * @inheritdoc PrivacyPool
    */
   function _push(address _recipient, uint256 _amount) internal override(PrivacyPool) {
-    /// Try to send native asset to recipient
-    (bool _success,) = _recipient.call{value: _amount}('');
-    if (!_success) revert FailedToSendNativeAsset();
+    // Send asset from this contract to recipient
+    IERC20(ASSET).safeTransfer(_recipient, _amount);
+  }
+
+  //Predefined delegate of this pool recieves all the vote delegation.
+  function delegate() external {
+    IERC20Votes(ASSET).delegate(delegateAddress);
   }
 }
