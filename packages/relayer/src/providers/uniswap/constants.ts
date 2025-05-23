2ce640682af0999be2d031d2b808c0ec404b8ac0
Francisco Bezzecchi <bezze@fatsolutions.xyz>
2025-05-22 23:47:44 -0300

    feat: swapping with relayer gas refund

--- packages/relayer/src/providers/uniswap/constants.ts
+++ packages/relayer/src/providers/uniswap/constants.ts
@@ -1,16 +1,22 @@
-import { Address } from "viem";
+import { Address, getAddress } from "viem";
+import { permit2Address } from "@uniswap/permit2-sdk";
+import { V3_CORE_FACTORY_ADDRESSES, QUOTER_ADDRESSES } from "@uniswap/sdk-core";
+import { UNIVERSAL_ROUTER_ADDRESS, UniversalRouterVersion } from "@uniswap/universal-router-sdk";
+
+export { WETH9 as WRAPPED_NATIVE_TOKEN_ADDRESS } from "@uniswap/sdk-core";
 
 /**
-* Mainnet, Polygon, Optimism, Arbitrum, Testnets Address
+* Mainnet (1), Polygon (137), Optimism (10), Arbitrum (42161), Testnets Address (11155111)
 * source: https://github.com/Uniswap/v3-periphery/blob/main/deploys.md
 */
-export const QUOTER_CONTRACT_ADDRESS: Record<string, Address> = {
-  "1": "0x61fFE014bA17989E743c5F6cB21bF9697530B21e",         // Ethereum
-  "137": "0x61fFE014bA17989E743c5F6cB21bF9697530B21e",       // polygon
-  "10": "0x61fFE014bA17989E743c5F6cB21bF9697530B21e",        // Optimism
-  "42161": "0x61fFE014bA17989E743c5F6cB21bF9697530B21e",     // Arbitrum
-  "11155111": "0xEd1f6473345F45b75F8179591dd5bA1888cf2FB3",  // Sepolia
-};
+
+// export const QUOTER_CONTRACT_ADDRESS: Record<string, Address> = {
+//   "1": "0x61fFE014bA17989E743c5F6cB21bF9697530B21e",         // Ethereum
+//   "137": "0x61fFE014bA17989E743c5F6cB21bF9697530B21e",       // polygon
+//   "10": "0x61fFE014bA17989E743c5F6cB21bF9697530B21e",        // Optimism
+//   "42161": "0x61fFE014bA17989E743c5F6cB21bF9697530B21e",     // Arbitrum
+//   "11155111": "0xEd1f6473345F45b75F8179591dd5bA1888cf2FB3",  // Sepolia
+// };
 
 export const FACTORY_CONTRACT_ADDRESS: Record<string, Address> = {
   "1": "0x1F98431c8aD98523631AE4a59f267346ea31F984",         // Ethereum
@@ -20,20 +26,18 @@ export const FACTORY_CONTRACT_ADDRESS: Record<string, Address> = {
   "11155111": "0x0227628f3f023bb0b980b67d528571c95c6dac1c",  // Sepolia
 }
 
-export const WRAPPED_NATIVE_TOKEN_ADDRESS: Record<string, Address> = {
-  "1": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",         // mainnet (WETH)
-  "137": "0x0000000000000000000000000000000000001010",       // polygon (POL)
-  // "137": "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270", // (WPOL) TODO: compare which token to use
-  "10": "0x4200000000000000000000000000000000000006",        // Optimism (WETH)
-  "42161": "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",     // Arbitrum (WETH)
-  "11155111": "0xfff9976782d46cc05630d1f6ebab18b2324d6b14",  // sepolia (WETH)
+export function getRouterAddress(chainId: number) {
+  return getAddress(UNIVERSAL_ROUTER_ADDRESS(UniversalRouterVersion.V2_0, chainId));
 }
 
-export const UNIVERSAL_ROUTER_ADDRESS: Record<string, Address> = {
-  "1": "0x66a9893cc07d91d95644aedd05d03f95e1dba8af",
-  "10": "0x851116d9223fabed8e56c0e6b8ad0c31d98b3507",
-  "137": "0x1095692a6237d83c6a72f3f5efedb9a670c49223",
-  "42161": "0xa51afafe0263b40edaef0df8781ea9aa03e381a3",
-  "11155111": "0x3a9d48ab9751398bbfa63ad67599bb04e4bdf98b"
+export function getPermit2Address(chainId: number) {
+  return getAddress(permit2Address(chainId));
 }
 
+export function getV3Factory(chainId: number) {
+  return getAddress(V3_CORE_FACTORY_ADDRESSES[chainId]!)
+}
+
+export function getQuoterAddress(chainId: number) {
+  return getAddress(QUOTER_ADDRESSES[chainId]!)
+}
--- packages/relayer/src/providers/uniswap/uniswap.provider.ts
+++ packages/relayer/src/providers/uniswap/uniswap.provider.ts
@@ -1,6 +1,6 @@
 import { Token } from '@uniswap/sdk-core';
 import { FeeAmount } from '@uniswap/v3-sdk';
-import { Address, getContract, createWalletClient, http, encodeAbiParameters } from 'viem';
+import { Address, getContract, createWalletClient, http, encodeAbiParameters, getAddress, SimulateContractReturnType, WriteContractReturnType, GetContractReturnType, SendTransactionParameters, WriteContractParameters } from 'viem';
 import { privateKeyToAccount } from 'viem/accounts';
 import { writeContract } from 'viem/actions';
 
@@ -8,10 +8,13 @@ import { getChainConfig, getSignerPrivateKey } from "../../config/index.js";
 import { web3Provider } from '../../providers/index.js';
 import { BlockchainError, RelayerError } from '../../exceptions/base.exception.js';
 import { isViemError } from '../../utils.js';
-import { UNIVERSAL_ROUTER_ADDRESS, QUOTER_CONTRACT_ADDRESS, WRAPPED_NATIVE_TOKEN_ADDRESS } from './constants.js';
+import { getRouterAddress, getQuoterAddress, WRAPPED_NATIVE_TOKEN_ADDRESS, getPermit2Address } from './constants.js';
 import { IERC20MinimalABI } from './abis/erc20.abi.js';
 import { QuoterV2ABI } from './abis/quoterV2.abi.js';
 import { UniversalRouterABI } from './abis/universalRouter.abi.js';
+import * as routerCommands from "./commands.js";
+import { getPoolPath } from './pools.js';
+import { createPermit2 } from './permit.js';
 
 export type UniswapQuote = {
   chainId: number;
@@ -21,17 +24,27 @@ export type UniswapQuote = {
 };
 
 type QuoteToken = { amount: bigint, decimals: number; };
+
 export type Quote = {
   in: QuoteToken;
   out: QuoteToken;
 };
+
+interface SwapWithRefundParams {
+  feeReceiver: `0x${string}`;
+  nativeRecipient: `0x${string}`;
+  tokenIn: `0x${string}`;
+  amountIn: bigint;
+  refundAmount: bigint;
+  chainId: number;
+}
 
 export class UniswapProvider {
 
   async getTokenInfo(chainId: number, address: Address): Promise<Token> {
     const contract = getContract({
       address,
-      abi: IERC20MinimalABI.abi,
+      abi: IERC20MinimalABI,
       client: web3Provider.client(chainId)
     });
     const [decimals, symbol] = await Promise.all([
@@ -42,11 +55,11 @@ export class UniswapProvider {
   }
 
   async quoteNativeToken(chainId: number, addressIn: Address, amountIn: bigint): Promise<Quote> {
-    const addressOut = WRAPPED_NATIVE_TOKEN_ADDRESS[chainId.toString()]!;
+    const weth = WRAPPED_NATIVE_TOKEN_ADDRESS[chainId]!;
     return this.quote({
       chainId,
       amountIn,
-      addressOut,
+      addressOut: weth.address,
       addressIn
     });
   }
@@ -55,8 +68,8 @@ export class UniswapProvider {
     const tokenIn = await this.getTokenInfo(chainId, addressIn as Address);
     const tokenOut = await this.getTokenInfo(chainId, addressOut as Address);
     const quoterContract = getContract({
-      address: QUOTER_CONTRACT_ADDRESS[chainId.toString()]!,
-      abi: QuoterV2ABI.abi,
+      address: getQuoterAddress(chainId),
+      abi: QuoterV2ABI,
       client: web3Provider.client(chainId)
     });
 
@@ -108,13 +121,12 @@ export class UniswapProvider {
     recipient: Address;
   }): Promise<string> {
     const config = getChainConfig(chainId);
-    const routerAddress = UNIVERSAL_ROUTER_ADDRESS[chainId.toString()];
+    const routerAddress = getRouterAddress(chainId);
     if (!routerAddress) {
       throw RelayerError.unknown(`Universal Router not configured for chain ${chainId}`);
     }
 
-    const privateKey = getSignerPrivateKey(chainId);
-    const account = privateKeyToAccount(privateKey as `0x${string}`);
+    const account = privateKeyToAccount(getSignerPrivateKey(chainId) as `0x${string}`);
 
     const client = createWalletClient({
       chain: {
@@ -160,7 +172,7 @@ export class UniswapProvider {
 
     const hash = await writeContract(client, {
       address: routerAddress,
-      abi: UniversalRouterABI.abi,
+      abi: UniversalRouterABI,
       functionName: 'execute',
       args: [commands, [encodedInput]],
       account
@@ -201,4 +213,135 @@ export class UniswapProvider {
       value: amount,
     });
   }
+
+  async approvePermit2forERC20(tokenIn: `0x${string}`, chainId: number) {
+    const relayer = privateKeyToAccount(getSignerPrivateKey(chainId) as `0x${string}`);
+    const PERMIT2_ADDRESS = getPermit2Address(chainId);
+    const client = web3Provider.client(chainId);
+    const erc20 = getContract({
+      abi: IERC20MinimalABI,
+      address: tokenIn,
+      client,
+    });
+    const allowance = await erc20.read.allowance([relayer.address, PERMIT2_ADDRESS]);
+    if (allowance < 2n ** 128n) {
+      await erc20.write.approve(
+        [PERMIT2_ADDRESS, 2n ** 256n - 1n],
+        { chain: client.chain, account: relayer }
+      );
+    }
+  }
+
+  // OPERATIONS:
+  //  0) - (this is done only once) - Approve Permit2 to move Relayer's ERC20
+  //  1) AllowanceTransfer from Relayer to Router
+  //  2) Swap ERC20 for WETH, destination Router, setting the permit2=true flag
+  //  3) Unwrap WETH to Router
+  //  4) Transfer Refund value to Relayer
+  //  5) Sweep whatever is left to Recipient
+  async swapExactInputSingleForWeth({ nativeRecipient, feeReceiver, tokenIn, amountIn, refundAmount, chainId }: SwapWithRefundParams): Promise<WriteContractParameters> {
+
+    const minAmountOut = 200_000n;
+    const ROUTER_ADDRESS = getRouterAddress(chainId);
+    const PERMIT2_ADDRESS = getPermit2Address(chainId);
+    const relayer = privateKeyToAccount(getSignerPrivateKey(chainId) as `0x${string}`);
+    const client = web3Provider.client(chainId);
+
+    const router = getContract({
+      abi: UniversalRouterABI,
+      address: ROUTER_ADDRESS,
+      client
+    });
+
+    const [permitSingle, signature] = await createPermit2({
+      signer: relayer,
+      chainId,
+      amountIn,
+      permit2Address: PERMIT2_ADDRESS,
+      routerAddress: ROUTER_ADDRESS,
+      assetAddress: tokenIn
+    });
+
+    // This is used to authorize the router to move our tokens
+    const permitCommandPair = routerCommands.permit2({ permit: permitSingle, signature });
+
+    const transferERC20NetFee = routerCommands.transfer({
+      // 0 address means moving native
+      token: getAddress("0x0000000000000000000000000000000000000000"),
+      recipient: relayer.address,
+      amount: refundAmount,
+    });
+
+    const pathParams = await getPoolPath(tokenIn, chainId);
+
+    // Swap consuming all
+    const swapCommandPAir = routerCommands.swapV3ExactIn({
+      // we're going to unwrap weth from here
+      recipient: router.address,
+      amountIn,
+      minAmountOut,
+      // USDC-WETH
+      path: pathParams,
+      // The relayer is the tx initiator
+      payerIsUser: true,
+    });
+
+    const unwrapOutputWeth = routerCommands.unwrapWeth({
+      // the router will hold the value for further splitting
+      recipient: router.address,
+      minAmountOut
+    });
+
+    const transferRefundNative = routerCommands.transfer({
+      // 0 address means moving native
+      token: getAddress("0x0000000000000000000000000000000000000000"),
+      recipient: relayer.address,
+      amount: refundAmount,
+    });
+
+    const sweepReminders = routerCommands.sweep({
+      // 0 address means moving native
+      token: getAddress("0x0000000000000000000000000000000000000000"),
+      // this is the withdrawal address
+      recipient: nativeRecipient,
+      minAmountOut
+    });
+
+    const commandPairs: [number, `0x${string}`][] = [
+      permitCommandPair,
+      swapCommandPAir,
+      unwrapOutputWeth,
+      transferRefundNative,
+      sweepReminders
+    ];
+
+    const commands = "0x" + commandPairs.map(x => x[0].toString(16).padStart(2, "0")).join("") as `0x${string}`;
+    const params = commandPairs.map(x => x[1]);
+
+    const { request: simulation } = await router.simulate.execute([commands, params]);
+
+    const estimateGas = await client.estimateContractGas(simulation);
+
+    const {
+      address,
+      abi,
+      functionName,
+      args,
+      chain,
+      nonce,
+    } = simulation;
+
+    return {
+      functionName,
+      account: relayer,
+      address,
+      abi,
+      args,
+      chain,
+      nonce,
+      gas: estimateGas * 15n / 10n
+    };
+
+  }
+
 }
