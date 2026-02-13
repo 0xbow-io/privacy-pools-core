add500717c55994eca94c3727170aef49026c45d
Patrick Aljord <patcito@gmail.com>
2026-02-13 18:27:11 +0100

    test(sdk): skip data service test when HYPERSYNC_API_KEY is not set

--- packages/sdk/test/unit/data.service.spec.ts
+++ packages/sdk/test/unit/data.service.spec.ts
@@ -5,7 +5,9 @@ import { Hash } from '../../src/types/commitment.js';
 import { DataError } from '../../src/errors/data.error.js';
 import { PoolInfo } from '../../src/types/account.js';
 
-describe('DataService with Sepolia', () => {
+const apiKey = process.env.HYPERSYNC_API_KEY;
+
+describe.skipIf(!apiKey)('DataService with Sepolia', () => {
   let dataService: DataService;
   const SEPOLIA_CHAIN_ID = 11155111;
   const POOL_ADDRESS = '0xbbe3b00d54f0ee032eff07a47139da8d44095c96';
@@ -28,16 +30,11 @@ describe('DataService with Sepolia', () => {
   };
 
   beforeAll(() => {
-    const apiKey = process.env.HYPERSYNC_API_KEY;
-    if (!apiKey) {
-      throw new Error('HYPERSYNC_API_KEY environment variable is not set. Please create a .env file with your API key.');
-    }
-
     const config: ChainConfig = {
       chainId: SEPOLIA_CHAIN_ID,
       privacyPoolAddress: POOL_ADDRESS,
       startBlock: START_BLOCK,
-      rpcUrl: `https://sepolia.rpc.hypersync.xyz/${apiKey}`,
+      rpcUrl: `https://sepolia.rpc.hypersync.xyz/${apiKey!}`,
     };
 
     dataService = new DataService([config]);
